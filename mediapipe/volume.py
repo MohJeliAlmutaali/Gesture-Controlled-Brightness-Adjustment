5a52c20e37da8b7d8758d9399ba0818c54fd1575
MohJeliAlmutaali <114728937+MohJeliAlmutaali@users.noreply.github.com>
2024-03-10 08:27:54 +0700

    Delete mediapipe directory

--- mediapipe/volume.py
+++
@@ -1,9 +0,0 @@
-import pulsectl
-
-def set_volume(volume):
-    with pulsectl.Pulse('set-volume-example') as pulse:
-        for sink in pulse.sink_list():
-            pulse.volume_set_all_chans(sink, volume)
-
-# Contoh penggunaan: atur volume menjadi 50% dari maksimum
-set_volume(0.2)  # Volume dalam rentang 0.0 hingga 1.0
